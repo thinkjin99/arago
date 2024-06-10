b9267cd22a32832517c3ce691b0aad20ae14c801
정명진 <thinkjin99@gmail.com>
2024-06-10 18:10:24 +0900

    [refactor] Remove duplicated code
    중복된 코드를 제거하고 주석을 추가했습니다.

--- arago/chatbot/teacher/views.py
+++ arago/chatbot/teacher/views.py
@@ -12,28 +12,28 @@ from chatbot.kakao import ChatbotView
 MAP_BLOCK_ID = "66667a153d1a600a8a3acc34"
 # Create your views here.
 class MapView(ChatbotView):
+    #약도 정보를 반환하는 API 입니다.
     def post(self, request):
         body = request.body.decode("utf-8")
         payload = json.loads(body)
         action = payload["action"]
-        parameters = action["params"]
-        extra_parameters = action["clientExtra"]
-        office_name = parameters.get("office_name") if parameters.get("office_name") else extra_parameters.get("office_name")
+        extra_parameters = action["clientExtra"] #선생님 상세 응답에서 버튼을 통해 전달되는 파라미터
+        office_name = extra_parameters.get("office_name")
         
         try:
             office_image_maps = {"예능환경":"https://wimg.mk.co.kr/news/cms/202312/15/news-p.v1.20231204.469decf4893d47f68cce5fc3bfbe0309_P1.jpg",
                                     "창의환경":"https://i.namu.wiki/i/zN7ASE4kyQNHO9jeAobgriDh2fqdbqiJVk5v7K-Tb_bCtOtem2v47wkFV4cQfYJYwbjr7bgoVqKVyHkp_Gy_6A.webp",
                                     "융합정보":"https://blog.kakaocdn.net/dn/15c1u/btrBF5rq2s1/uCDT0O1GSpm5WEu8kHzna0/img.jpg"} #교무실 별 약도
         
-            if office_name:
+            if office_name: #버튼을 통해 접근한 경우
                 outputs = {"simpleImage":{"imageUrl": "","altText":"교무실 약도 입니다"}}
                 image_url = office_image_maps.get(office_name)
                 assert image_url,"교무실 이름이 정확하지 않아요" #교무실 이름이 잘못됐습니다.
                 outputs["simpleImage"]["imageUrl"] = image_url
                 
-            else:
+            else: #버튼이 아닌 키워드로 접속한 경우
                 office_names = office_image_maps.keys()
-                text_card_template = {
+                outputs = {
                     "textCard":{
                     "title":"교무실을 지정해주세요",
                     "description":"위치를 확인할 교무실을 지정해주세요",
@@ -42,11 +42,11 @@ class MapView(ChatbotView):
                 }
                 button_template = {
                                 "action": "block",
-                                "blockId": MAP_BLOCK_ID,
+                                "blockId": MAP_BLOCK_ID, #연결할 블락 아이디
                                 "label": "",
-                                "extra": {"office_name": ""},
+                                "extra": {"office_name": ""}, #블락에 전달할 파라미터
                             }
-                buttons = text_card_template["textCard"]["buttons"] 
+                buttons = outputs["textCard"]["buttons"] 
                 
                 for office_name in office_names:
                     new_button = deepcopy(button_template)
@@ -54,9 +54,6 @@ class MapView(ChatbotView):
                     new_button["extra"]["office_name"] = office_name
                     buttons.append(new_button)
                 
-                outputs = text_card_template
-
-        
         except Exception as e:
             outputs = {"simpleText": {"text": str(e)}}
 
@@ -72,19 +69,19 @@ class TeacherView(ChatbotView):
         "blockId": MAP_BLOCK_ID,
         "label": "약도보기",
         "extra": {"office_name": ""},
-    }
+    } #카드에 추가할 버튼 템플릿
 
     card_template = {
         "title": "",
         "description": "선생님 정보",
         "itemList": [],
         "buttons": [],
-    }
+    } #카드 템플릿
 
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        if self.action == "all":
+        if self.action == "all": #전체 선생님 리스트
             resp = self.post_all()
-        elif self.action == "name":
+        elif self.action == "name": #선생님 세부
             resp = self.post_name(request)
         return resp
 
@@ -92,17 +89,14 @@ class TeacherView(ChatbotView):
         try:
             body = request.body.decode("utf-8")
             payload = json.loads(body)
-            patt = r"[가-힣a-zA-Z]+"
             parameters = payload["action"]["params"]
-
             name_parameter = parameters.get("teacher_name")
             assert name_parameter, "선생님 이름을 입력해 주세요."
 
-            match_result = re.match(patt, name_parameter)
+            match_result = re.match(r"[가-힣a-zA-Z]+", name_parameter) #이름 형식만 매칭
             assert match_result, "이름 형식이 아닌것 같아요. 이름을 입력해 주세요."
-
-            teacher_name = match_result.group()
-            query_sets = Teacher.objects.filter(name=teacher_name).values(
+            
+            query_sets = Teacher.objects.filter(name=match_result.group()).values(
                 "name", "subject", "office_name", "floor"
             )  # 동명이인이 있을 수도 있으니까 배열로 반환
 
@@ -119,10 +113,10 @@ class TeacherView(ChatbotView):
                 new_card["buttons"].append(new_button)
 
                 new_card["title"] = row["name"] + " 선생님"
-                items = [{"title": key, "description": row[key]} for key in row]
+                items = [{"title": key, "description": row[key]} for key in row] #선생님 정보 추가
                 new_card["itemList"] = items
 
-                outputs.append({"itemCard":new_card})
+                outputs.append({"itemCard":new_card}) #아이템 카드로 추가
 
         except Exception as e:
             outputs = {"simpleText": {"text": str(e)}}
